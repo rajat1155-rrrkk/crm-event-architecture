8aa595b8d90d5e2e4ec45b98826af16bcc073ffb
Rajat K <rajat.khitoliya15@gmail.com>
2026-02-20 16:21:21 +0530

    feat: integrate retry handler and dead letter queue into event flow

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from event_bus.event_bus import EventBus
 from publishers.lead_publisher import LeadPublisher
 from consumers.lead_consumer import LeadConsumer
+from retry_handler.retry_handler import RetryHandler
+from dead_letter.dead_letter_queue import DeadLetterQueue
 
 
 def run_demo():
     bus = EventBus()
+    retry_handler = RetryHandler(max_retries=2, delay=1)
+    dlq = DeadLetterQueue()
 
     consumer = LeadConsumer()
-    bus.subscribe("LeadCreated", consumer.handle_lead_created)
+
+    def wrapped_handler(data):
+        try:
+            retry_handler.execute(consumer.handle_lead_created, data)
+        except Exception:
+            dlq.add("LeadCreated", data)
+
+    bus.subscribe("LeadCreated", wrapped_handler)
 
     publisher = LeadPublisher(bus)
     publisher.create_lead({"name": "Enterprise Corp", "industry": "FinTech"})
